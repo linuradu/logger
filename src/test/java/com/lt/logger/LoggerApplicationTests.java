996d49a3796ab47059cb34f5d96a62489a832775
Radu Linu <radu.linu@mateco.eu>
2023-05-10 20:44:01 +0300

    Initial commit

---
+++ src/test/java/com/lt/logger/LoggerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.lt.logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
